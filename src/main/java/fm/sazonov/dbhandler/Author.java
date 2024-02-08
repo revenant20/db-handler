606d9129117d9c7f3df10f4f3c1850f41221dcdd
sazonovfm <sazonovfm@gmail.com>
2024-02-08 21:08:01 +0300

    добавил таблицу автора и отношение один ко многим

---
+++ src/main/java/fm/sazonov/dbhandler/Author.java
@@ -0,0 +1,34 @@
+package fm.sazonov.dbhandler;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid2")
+    @Column(name = "author_id")
+    private String id;
+
+    @OneToMany(
+            mappedBy = "author"
+    )
+    @ToString.Exclude
+    private List<Book> books;
+}
--- src/main/java/fm/sazonov/dbhandler/Book.java
+++ src/main/java/fm/sazonov/dbhandler/Book.java
@@ -5,11 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
@@ -22,4 +18,8 @@ public class Book {
     @GeneratedValue(generator = "uuid2")
     @Column(name = "book_id")
     private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
 }
